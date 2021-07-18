6e3072a25a6942ea5da97fccbd2f19ac07c56939
Chirtle <10554267+eating-coleslaw@users.noreply.github.com>
2021-07-18 12:35:56 -0500

    Add basic material-ui grid layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,92 @@
-import { useState } from 'react';
-import { TextField } from '@material-ui/core';
-import '../styles/App.css';
+import React, { useState } from "react";
+import {
+  createTheme,
+  ThemeProvider,
+  makeStyles,
+} from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { Container, Grid, Paper } from "@material-ui/core";
+import "../styles/App.css";
+import ServiceKeyForm from "./ServiceKeyForm";
+import { pink, amber } from "@material-ui/core/colors";
 
-const dbgcensus = require('dbgcensus');
+const dbgcensus = require("dbgcensus");
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  grid: {
+    padding: theme.spacing(2),
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+  header1: {
+    margin: `0 0 ${theme.spacing(1)}px 0`,
+  },
+}));
 
 function App() {
-  dbgcensus.SetGlobalNamespace('ps2:v2');
-  
-  const [serviceKey, setServiceKey] = useState('example');
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          type: "light", //prefersDarkMode ? "dark" : "light",
+          primary: pink,
+          secondary: amber,
+        },
+        contrastThreshold: 5,
+      }),
+    [prefersDarkMode]
+  );
 
-  const onSubmitQuery = (formServiceKey) => {
-    dbgcensus.SetGlobalServiceKey(formServiceKey);
-    
-    setServiceKey(formServiceKey);
+  dbgcensus.SetGlobalNamespace("ps2:v2");
 
-    console.log(dbgcensus.serviceKey);
+  const [serviceKey, setServiceKey] = useState("example");
+
+  function onServiceKeyChange(key) {
+    // const value = e.target.value;
+
+    const value = key;
+
+    dbgcensus.SetGlobalServiceKey(value);
+
+    setServiceKey(value);
+
+    console.log(value);
   }
 
+  const classes = useStyles();
   return (
-    <div>
-      <form noValidate autoComplete="off">
-        <TextField id="service-key" label="Service Key" required onChange={onSubmitQuery} placeholder="example" value={serviceKey}/>
-        <TextField id="other-field" label="Other Field" required onChange={() => {return null}}/>
-        {/* <Button onClick={onSubmitQuery} */}
-      </form>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className={classes.root}>
+        <Grid container className={classes.grid}>
+          <Grid item xs={12} sm={6} className={classes.grid}>
+            <Paper className={classes.paper}>
+              <h1 className={classes.header1}>Query Creator</h1>
+              <ServiceKeyForm onServiceKeyChange={onServiceKeyChange} />
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Grid item xs={12} className={classes.grid}>
+              <Paper className={classes.paper}>
+                <h1 className={classes.header1}>Query String</h1>
+              </Paper>
+            </Grid>
+            <Grid item xs={12} className={classes.grid}>
+              <Paper className={classes.paper}>
+                <h1 className={classes.header1}>Query Results</h1>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/ServiceKeyForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
+import SaveIcon from "@material-ui/icons/Save";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+function ServiceKeyForm({ onServiceKeyChange }) {
+  const [serviceKey, setServiceKey] = useState("example"); // TODO: get service key from localStorage
+
+  function onSubmitForm(e) {
+    e.preventDefault();
+    onServiceKeyChange(serviceKey);
+  }
+
+  const classes = useStyles();
+
+  return (
+    <form noValidate autoComplete="off" onSubmit={onSubmitForm}>
+      <TextField
+        id="service-key"
+        label="Service Key"
+        required
+        name="service-key"
+        onChange={(e) => setServiceKey(e.target.value)}
+        placeholder="example"
+        value={serviceKey}
+      />
+      <Button
+        type="submit"
+        variant="outlined"
+        color="primary"
+        size="large"
+        className={classes.button}
+        startIcon={<SaveIcon />}
+      >
+        Save
+      </Button>
+    </form>
+  );
+}
+
+export default ServiceKeyForm;
