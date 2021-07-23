009670cf4e706efa2c2a9d49605b7df555d78492
Chirtle <10554267+eating-coleslaw@users.noreply.github.com>
2021-07-23 09:32:34 -0500

    Add 'Copy' button to Query Results

--- src/components/queries/QueryResults.js
+++ src/components/queries/QueryResults.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { LinearProgress, Paper } from "@material-ui/core";
-import ReactJson from "react-json-view";
+import { LinearProgress, Paper, Grid, Button } from "@material-ui/core";
+import AssignmentIcon from "@material-ui/icons/Assignment";
 
 const useStyles = makeStyles((theme) => ({
+  headerButtonContainer: {
+    textAlign: "right",
+  },
   header1: {
     margin: 0,
     fontSize: "1.4em",
-    // color: theme.palette.text.primary,
     fontWeight: 500,
   },
   paper: {
@@ -28,41 +30,51 @@ const useStyles = makeStyles((theme) => ({
   linearProgres: {
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(2),
-  }
+  },
 }));
 
 export default function QueryResults({ data, isLoading }) {
   const classes = useStyles();
 
+  function copyDataToClipboard() {
+    if (!!data) {
+      navigator.clipboard.writeText(JSON.stringify(data, null, 2));
+    }
+  }
+
   if (isLoading) {
     return (
       <Paper className={classes.paper}>
         <h1>Query Results</h1>
-        <LinearProgress className={classes.linearProgres}/>
+        <LinearProgress className={classes.linearProgres} />
       </Paper>
     );
   }
 
   return (
     <Paper className={classes.paper}>
-      <h1 className={classes.header1}>Query Results</h1>
+      <Grid container justifyContent="space-between" alignItems="center">
+        <Grid item xs={8}>
+          <h1 className={classes.header1}>Query Results</h1>
+        </Grid>
+        <Grid item xs={4} className={classes.headerButtonContainer}>
+          {!!data && (
+            <Button
+              color="primary"
+              onClick={copyDataToClipboard}
+              value="Copy"
+              title="Copy the query results to the clipboard"
+              startIcon={<AssignmentIcon />}
+            >
+              Copy
+            </Button>
+          )}
+        </Grid>
+      </Grid>
       {!!data ? (
         <div className={classes.textView}>
           <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
-        /*{/* <ReactJson
-          src={data}
-          enableDelete={true}
-          iconStyle="circle"
-          displayObjectSize={false}
-          displayDataTypes={false}
-          style={{
-            maxHeight: "600px",
-            overflow: "auto",
-            lineHeight: "1.1",
-            marginTop: 8,
-          }}
-        /> }*/
       ) : null}
     </Paper>
   );
