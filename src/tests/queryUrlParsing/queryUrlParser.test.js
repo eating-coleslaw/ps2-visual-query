cd3ac05af904e59194d2903f28d5c5805eb05f0d
Chirtle <10554267+eating-coleslaw@users.noreply.github.com>
2021-08-12 10:36:26 -0500

    clean up comments

--- src/tests/queryUrlParsing/queryUrlParser.test.js
+++ src/tests/queryUrlParsing/queryUrlParser.test.js
@@ -1,36 +1,24 @@
-import { v4 as uuid } from 'uuid';
-import QueryCondition from '../../planetside/QueryCondition';
-import QueryConfig from '../../planetside/QueryConfig';
-import QueryJoin from '../../planetside/QueryJoin';
-import QueryOperator from '../../planetside/QueryOperator';
-import parseQueryUrl from '../../planetside/queryUrlParsing/queryUrlParser';
+import { v4 as uuid } from "uuid";
+import QueryCondition from "../../planetside/QueryCondition";
+import QueryConfig from "../../planetside/QueryConfig";
+import QueryJoin from "../../planetside/QueryJoin";
+import QueryOperator from "../../planetside/QueryOperator";
+import parseQueryUrl from "../../planetside/queryUrlParsing/queryUrlParser";
 
-// jest.mock('uuid/v4');
-jest.mock('uuid');
+jest.mock("uuid");
 
 const mockId = "1094";
 uuid.mockImplementation(() => mockId);
 
-// test("Testing Jest mocks", () => {
-//   const id = "1094";
-//   uuid.mockImplementation(() => id);
-
-//   console.log(uuid());
-// });
-
 test("Inner join example", () => {
-  const input = "https://census.daybreakgames.com/get/ps2/map?world_id=1&zone_ids=2&c:join=map_region^inject_at:map_region^on:Regions.Row.RowData.RegionId^to:map_region_id";
+  const input =
+    "https://census.daybreakgames.com/get/ps2/map?world_id=1&zone_ids=2&c:join=map_region^inject_at:map_region^on:Regions.Row.RowData.RegionId^to:map_region_id";
 
   const mockId = "1094";
   uuid.mockImplementation(() => mockId);
 
-  // const mockId = "1094";
-
-  // uuid.mockImplementation(() => mockId);
   const expected = QueryConfig("map");
-  
-  // expected.id = mockId;
-  
+
   const worldIdCondition = QueryCondition("equals");
   worldIdCondition.field = "world_id";
   worldIdCondition.value = "1";
@@ -39,22 +27,7 @@ test("Inner join example", () => {
   zoneIdsCondition.field = "zone_ids";
   zoneIdsCondition.value = "2";
 
-  expected.conditions = [ worldIdCondition, zoneIdsCondition ];
-
-  // expected.conditions = [
-  //   {
-  //     id: mockId,
-  //     field: "world_id",
-  //     value: "1",
-  //     operator: QueryOperator("equals")
-  //   },
-  //   {
-  //     id: mockId,
-  //     field: "zone_ids",
-  //     value: "2",
-  //     operator: QueryOperator("equals")
-  //   },
-  // ];
+  expected.conditions = [worldIdCondition, zoneIdsCondition];
 
   const rootJoin = QueryJoin();
   rootJoin.collection = "map_region";
@@ -62,32 +35,31 @@ test("Inner join example", () => {
   rootJoin.onField = "Regions.Row.RowData.RegionId";
   rootJoin.toField = "map_region_id";
 
-  expected.joins = [ rootJoin ];
+  expected.joins = [rootJoin];
 
   expect(parseQueryUrl(input)).toEqual(expected);
 });
 
 test("Online Outfit Members example", () => {
-  const input = "http://census.daybreakgames.com/get/ps2/outfit?alias_lower=mums&c:show=name,outfit_id&c:join=outfit_member^inject_at:members^show:character_id%27rank^outer:0^list:1(character^show:name.first^inject_at:character^outer:0^on:character_id(characters_online_status^inject_at:online_status^show:online_status^outer:0(world^on:online_status^to:world_id^outer:0^show:world_id^inject_at:ignore_this)))";
+  const input =
+    "http://census.daybreakgames.com/get/ps2/outfit?alias_lower=mums&c:show=name,outfit_id&c:join=outfit_member^inject_at:members^show:character_id%27rank^outer:0^list:1(character^show:name.first^inject_at:character^outer:0^on:character_id(characters_online_status^inject_at:online_status^show:online_status^outer:0(world^on:online_status^to:world_id^outer:0^show:world_id^inject_at:ignore_this)))";
 
   const mockId = "1094";
   uuid.mockImplementation(() => mockId);
 
   const expected = QueryConfig("outfit");
 
-  // expected.id = mockId;
-
   expected.conditions = [
     {
       id: mockId,
       field: "alias_lower",
       value: "mums",
-      operator: QueryOperator("equals")
+      operator: QueryOperator("equals"),
     },
   ];
 
   expected.filterType = "show";
-  expected.filterFields = [ "name", "outfit_id" ];
+  expected.filterFields = ["name", "outfit_id"];
 
   /*
     join=outfit_member^inject_at:members^show:character_id'rank^outer:0^list:1
@@ -98,26 +70,26 @@ test("Online Outfit Members example", () => {
       )
   */
   const rootJoin = QueryJoin();
-  rootJoin.collection = "outfit_member"
+  rootJoin.collection = "outfit_member";
   rootJoin.injectAt = "members";
   rootJoin.filterType = "show";
-  rootJoin.filterFields = [ "character_id", "rank" ];
+  rootJoin.filterFields = ["character_id", "rank"];
   rootJoin.isOuterJoin = false;
   rootJoin.isList = true;
 
   const firstChildJoin = QueryJoin(mockId);
   firstChildJoin.collection = "character";
   firstChildJoin.filterType = "show";
-  firstChildJoin.filterFields = [ "name.first" ];
+  firstChildJoin.filterFields = ["name.first"];
   firstChildJoin.injectAt = "character";
   firstChildJoin.isOuterJoin = false;
   firstChildJoin.onField = "character_id";
-  
+
   const secondChildJoin = QueryJoin(mockId);
   secondChildJoin.collection = "characters_online_status";
   secondChildJoin.injectAt = "online_status";
   secondChildJoin.filterType = "show";
-  secondChildJoin.filterFields = [ "online_status" ];
+  secondChildJoin.filterFields = ["online_status"];
   secondChildJoin.isOuterJoin = false;
 
   const lastChildJoin = QueryJoin(mockId);
@@ -126,22 +98,23 @@ test("Online Outfit Members example", () => {
   lastChildJoin.toField = "world_id";
   lastChildJoin.isOuterJoin = false;
   lastChildJoin.filterType = "show";
-  lastChildJoin.filterFields = [ "world_id" ];
+  lastChildJoin.filterFields = ["world_id"];
   lastChildJoin.injectAt = "ignore_this";
 
-  secondChildJoin.joins = [ lastChildJoin ];
+  secondChildJoin.joins = [lastChildJoin];
 
-  firstChildJoin.joins = [ secondChildJoin ];
+  firstChildJoin.joins = [secondChildJoin];
 
-  rootJoin.joins = [ firstChildJoin ];
+  rootJoin.joins = [firstChildJoin];
 
-  expected.joins = [ rootJoin ];
+  expected.joins = [rootJoin];
 
   expect(parseQueryUrl(input)).toEqual(expected);
 });
 
 test("Cooltrain's chonky character", () => {
-  const input = "https://census.daybreakgames.com/s:example/get/ps2:v2/character/?name.first_lower=chirtle&c:hide=head_id,title_id,profile_id,times.creation,times.creation_date,times.last_save,times.last_save_date,times.last_login,times.login_count,daily_ribbon,certs.gifted_points,certs.spent_points,certs.available_points,certs.percent_to_next,battle_rank.percent_to_next&c:resolve=online_status&c:join=characters_stat_history^terms:stat_name=kills^hide:character_id'day'week'month'last_save'last_save_date'one_life_max^inject_at:Stats.History.Kills&c:join=characters_stat_history^terms:stat_name=deaths^hide:character_id'day'week'month'last_save'last_save_date'one_life_max^inject_at:Stats.History.Deaths&c:join=characters_stat_history^terms:stat_name=score^hide:character_id'day'week'month'last_save'last_save_date'one_life_max^inject_at:Stats.History.Score&c:join=faction^inject_at:faction^show:name.en'faction_id&c:join=outfit_member_extended^inject_at:outfit^show:name'alias&c:join=characters_weapon_stat^list:1^inject_at:Stats.WeaponStat(item^inject_at:item^show:faction_id'name.en^terms:faction_id=3^outer:0),characters_weapon_stat_by_faction^list:1^inject_at:Stats.WeaponStatFaction(item^inject_at:item^show:faction_id'name.en^terms:faction_id=3^outer:0)";
+  const input =
+    "https://census.daybreakgames.com/s:example/get/ps2:v2/character/?name.first_lower=chirtle&c:hide=head_id,title_id,profile_id,times.creation,times.creation_date,times.last_save,times.last_save_date,times.last_login,times.login_count,daily_ribbon,certs.gifted_points,certs.spent_points,certs.available_points,certs.percent_to_next,battle_rank.percent_to_next&c:resolve=online_status&c:join=characters_stat_history^terms:stat_name=kills^hide:character_id'day'week'month'last_save'last_save_date'one_life_max^inject_at:Stats.History.Kills&c:join=characters_stat_history^terms:stat_name=deaths^hide:character_id'day'week'month'last_save'last_save_date'one_life_max^inject_at:Stats.History.Deaths&c:join=characters_stat_history^terms:stat_name=score^hide:character_id'day'week'month'last_save'last_save_date'one_life_max^inject_at:Stats.History.Score&c:join=faction^inject_at:faction^show:name.en'faction_id&c:join=outfit_member_extended^inject_at:outfit^show:name'alias&c:join=characters_weapon_stat^list:1^inject_at:Stats.WeaponStat(item^inject_at:item^show:faction_id'name.en^terms:faction_id=3^outer:0),characters_weapon_stat_by_faction^list:1^inject_at:Stats.WeaponStatFaction(item^inject_at:item^show:faction_id'name.en^terms:faction_id=3^outer:0)";
 
   const mockId = "1094";
   uuid.mockImplementation(() => mockId);
@@ -159,7 +132,7 @@ test("Cooltrain's chonky character", () => {
   nameCondition.field = "name.first_lower";
   nameCondition.value = "chirtle";
 
-  expected.conditions = [ nameCondition ];
+  expected.conditions = [nameCondition];
 
   // expected.conditions = [
   //   {
@@ -171,9 +144,25 @@ test("Cooltrain's chonky character", () => {
   // ];
 
   expected.filterType = "hide";
-  expected.filterFields = [ "head_id", "title_id", "profile_id", "times.creation", "times.creation_date", "times.last_save", "times.last_save_date", "times.last_login", "times.login_count", "daily_ribbon", "certs.gifted_points", "certs.spent_points", "certs.available_points", "certs.percent_to_next", "battle_rank.percent_to_next" ];
+  expected.filterFields = [
+    "head_id",
+    "title_id",
+    "profile_id",
+    "times.creation",
+    "times.creation_date",
+    "times.last_save",
+    "times.last_save_date",
+    "times.last_login",
+    "times.login_count",
+    "daily_ribbon",
+    "certs.gifted_points",
+    "certs.spent_points",
+    "certs.available_points",
+    "certs.percent_to_next",
+    "battle_rank.percent_to_next",
+  ];
 
-  expected.resolves = [ "online_status" ];
+  expected.resolves = ["online_status"];
 
   /*
     join=characters_stat_history^terms:stat_name=kills^hide:character_id'day'week'month'last_save'last_save_date'one_life_max^inject_at:Stats.History.Kills
@@ -189,24 +178,32 @@ test("Cooltrain's chonky character", () => {
 
   // characters_stat_history^terms:stat_name=kills^hide:character_id'day'week'month'last_save'last_save_date'one_life_max^inject_at:Stats.History.Kills
   const killsRootJoin = QueryJoin();
-  killsRootJoin.collection = "characters_stat_history"
+  killsRootJoin.collection = "characters_stat_history";
   killsRootJoin.terms = [
     {
       id: mockId,
       field: "stat_name",
       value: "kills",
       operator: QueryOperator("equals"),
-    }
+    },
   ];
   killsRootJoin.filterType = "hide";
-  killsRootJoin.filterFields = [ "character_id", "day", "week", "month", "last_save", "last_save_date", "one_life_max" ];
+  killsRootJoin.filterFields = [
+    "character_id",
+    "day",
+    "week",
+    "month",
+    "last_save",
+    "last_save_date",
+    "one_life_max",
+  ];
   killsRootJoin.injectAt = "Stats.History.Kills";
 
   expected.joins.push(killsRootJoin);
 
   // characters_stat_history^terms:stat_name=deaths^hide:character_id'day'week'month'last_save'last_save_date'one_life_max^inject_at:Stats.History.Deaths
   const deathsRootJoin = QueryJoin();
-  deathsRootJoin.collection = "characters_stat_history"
+  deathsRootJoin.collection = "characters_stat_history";
   deathsRootJoin.terms = [
     {
       id: mockId,
@@ -216,14 +213,22 @@ test("Cooltrain's chonky character", () => {
     },
   ];
   deathsRootJoin.filterType = "hide";
-  deathsRootJoin.filterFields = [ "character_id", "day", "week", "month", "last_save", "last_save_date", "one_life_max" ];
+  deathsRootJoin.filterFields = [
+    "character_id",
+    "day",
+    "week",
+    "month",
+    "last_save",
+    "last_save_date",
+    "one_life_max",
+  ];
   deathsRootJoin.injectAt = "Stats.History.Deaths";
 
   expected.joins.push(deathsRootJoin);
 
   // characters_stat_history^terms:stat_name=score^hide:character_id'day'week'month'last_save'last_save_date'one_life_max^inject_at:Stats.History.Score
   const scoreRootJoin = QueryJoin();
-  scoreRootJoin.collection = "characters_stat_history"
+  scoreRootJoin.collection = "characters_stat_history";
   scoreRootJoin.terms = [
     {
       id: mockId,
@@ -233,7 +238,15 @@ test("Cooltrain's chonky character", () => {
     },
   ];
   scoreRootJoin.filterType = "hide";
-  scoreRootJoin.filterFields = [ "character_id", "day", "week", "month", "last_save", "last_save_date", "one_life_max" ];
+  scoreRootJoin.filterFields = [
+    "character_id",
+    "day",
+    "week",
+    "month",
+    "last_save",
+    "last_save_date",
+    "one_life_max",
+  ];
   scoreRootJoin.injectAt = "Stats.History.Score";
 
   expected.joins.push(scoreRootJoin);
@@ -243,7 +256,7 @@ test("Cooltrain's chonky character", () => {
   factionRootJoin.collection = "faction";
   factionRootJoin.injectAt = "faction";
   factionRootJoin.filterType = "show";
-  factionRootJoin.filterFields = [ "name.en", "faction_id" ]; 
+  factionRootJoin.filterFields = ["name.en", "faction_id"];
 
   expected.joins.push(factionRootJoin);
 
@@ -252,7 +265,7 @@ test("Cooltrain's chonky character", () => {
   outfitRootJoin.collection = "outfit_member_extended";
   outfitRootJoin.injectAt = "outfit";
   outfitRootJoin.filterType = "show";
-  outfitRootJoin.filterFields = [ "name", "alias" ];
+  outfitRootJoin.filterFields = ["name", "alias"];
 
   expected.joins.push(outfitRootJoin);
 
@@ -267,18 +280,18 @@ test("Cooltrain's chonky character", () => {
   weaponChildJoin.collection = "item";
   weaponChildJoin.injectAt = "item";
   weaponChildJoin.filterType = "show";
-  weaponChildJoin.filterFields = [ "faction_id", "name.en" ];
+  weaponChildJoin.filterFields = ["faction_id", "name.en"];
   weaponChildJoin.isOuterJoin = false;
   weaponChildJoin.terms = [
     {
       id: mockId,
       field: "faction_id",
       value: "3",
-      operator: QueryOperator("equals") 
+      operator: QueryOperator("equals"),
     },
   ];
 
-  weaponsRootJoin.joins = [ weaponChildJoin ];
+  weaponsRootJoin.joins = [weaponChildJoin];
 
   expected.joins.push(weaponsRootJoin);
 
@@ -293,18 +306,18 @@ test("Cooltrain's chonky character", () => {
   weaponFactionChildJoin.collection = "item";
   weaponFactionChildJoin.injectAt = "item";
   weaponFactionChildJoin.filterType = "show";
-  weaponFactionChildJoin.filterFields = [ "faction_id", "name.en" ];
+  weaponFactionChildJoin.filterFields = ["faction_id", "name.en"];
   weaponFactionChildJoin.isOuterJoin = false;
   weaponFactionChildJoin.terms = [
     {
       id: mockId,
       field: "faction_id",
       value: "3",
-      operator: QueryOperator("equals") 
+      operator: QueryOperator("equals"),
     },
   ];
 
-  weaponFactionRootJoin.joins = [ weaponFactionChildJoin ];
+  weaponFactionRootJoin.joins = [weaponFactionChildJoin];
 
   expected.joins.push(weaponFactionRootJoin);
 
